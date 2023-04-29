7446df7446ef1b4448d7f40db4e3d67cebfcc29c
Rubens Lyra <rubensvlyra@gmail.com>
2023-04-29 18:51:45 -0300

    feat: base da aplicação criada

---
+++ crud-csharp-sqlserver/DbConnection.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace SingletonCRUD
+{
+    public class DbConnection : IDisposable
+    {
+        private static DbConnection instance;
+        private readonly SqlConnection connection;
+
+        private DbConnection()
+        {
+            string connectionString = @"Data Source=localhost,1433;Initial Catalog=AppSingleton;User ID=sa;Password=Local@123456789;";
+            connection = new SqlConnection(connectionString);
+            connection.Open();
+        }
+
+        public static DbConnection Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new DbConnection();
+                }
+                return instance;
+            }
+        }
+
+        public SqlConnection GetConnection()
+        {
+            return connection;
+        }
+
+        public void Dispose()
+        {
+            connection.Close();
+        }
+
+    }
+}
